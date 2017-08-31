38bb559ab0d0fa0c247a1a28b6285db04a2423ac
p0wer0n <p0wer0n@users.noreply.github.com>
2017-08-30 20:18:11 -0400

    Add files

---
+++ ziva3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ziva3
+//
+//  Created by Jared Ebenstein on 8/30/17.
+//  Copyright © 2017 Jared Ebenstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ziva3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ziva3
+//
+//  Created by Jared Ebenstein on 8/30/17.
+//  Copyright © 2017 Jared Ebenstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
